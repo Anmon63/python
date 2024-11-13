ef9fa04a48632f21722ac4985da2ae99c2213473
Anmon63 <122660516+Anmon63@users.noreply.github.com>
2024-11-13 13:14:31 +0530

    Add files via upload

---
+++ Task59/t.js
@@ -0,0 +1,8 @@
+var data={
+    "name":"Christ",
+    "Place":"Israel",
+    "Age":33
+}
+var a = (data.name)+" is "+(data.Age)+" years old and lives in "+(data.Place)
+console.log(a)
+document.write(a)
---
+++ Task60/t.js
@@ -0,0 +1,3 @@
+let hands = ["rock", "paper", "scissor"]
+let a=hands[Math.floor(Math.random() * hands.length)]
+document.write(a)
