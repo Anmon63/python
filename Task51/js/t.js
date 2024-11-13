037a931ed4c3baf77fe5f208dc7d895baee5ef41
Anmon63 <122660516+Anmon63@users.noreply.github.com>
2024-11-13 13:04:16 +0530

    Add files via upload

---
+++ Task51/js/t.js
@@ -0,0 +1,10 @@
+function img1(){
+  
+    document.getElementById("img").innerHTML = `<img src="image/img1.png">`;
+}
+function img2(){
+    document.getElementById("img").innerHTML = `<img src="image/img2.png">`;
+}
+function img3(){
+    document.getElementById("img").innerHTML = `<img src="image/img3.png">`;
+}
